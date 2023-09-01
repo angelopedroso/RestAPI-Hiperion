ebaf15e8943697c8a09e25457984b2fc00527d51
angelopedroso <solonasusonly0@gmail.com>
2023-09-01 17:31:10 -0300

    fix: fixed update group endpoint

--- src/database/controller/GroupController.ts
+++ src/database/controller/GroupController.ts
@@ -1,6 +1,18 @@
 import { Request, Response } from 'express'
 import { prisma, redis } from '@/index'
 
+type Participant = {
+  id: string
+  p_id: string
+  image_url: string | null
+  name: string
+}
+
+type AntiTrava = {
+  id: string
+  status: boolean | null
+  max_characters: number | null
+}
 interface GroupBody {
   _id: string
   g_id: string
@@ -12,6 +24,8 @@ interface GroupBody {
   anti_porn: boolean
   name: string
   image_url: string | null
+  anti_trava: AntiTrava
+  black_list: Participant[]
 }
 
 class GroupController {
@@ -83,23 +97,47 @@ class GroupController {
         auto_invite_link: autoInviteLink,
         auto_sticker: autoSticker,
         bem_vindo: bemVindo,
-        g_id: groupId,
+        black_list: blackList,
+        anti_trava: antiTrava,
         one_group: oneGroup,
       }: GroupBody = request.body
 
-      const group = await GroupModel.findByIdAndUpdate(
-        id,
-        {
-          antiLink,
-          antiPorn,
-          autoInviteLink,
-          autoSticker,
-          bemVindo,
-          groupId,
-          oneGroup,
+      const existsGroup = await prisma.group.findUnique({
+        where: {
+          id,
         },
-        { new: true },
-      )
+      })
+
+      if (!existsGroup) {
+        return response.status(400).json({
+          message: 'Group not found!',
+        })
+      }
+
+      const group = await prisma.group.update({
+        where: {
+          id,
+        },
+        data: {
+          bem_vindo: bemVindo,
+          anti_link: antiLink,
+          anti_porn: antiPorn,
+          one_group: oneGroup,
+          auto_sticker: autoSticker,
+          auto_invite_link: autoInviteLink,
+          anti_trava: {
+            update: {
+              status: antiTrava?.status,
+              max_characters: antiTrava?.max_characters,
+            },
+          },
+          // black_list: {
+          //   connect: blackList?.map((user) => ({
+          //     id: user.id,
+          //   })),
+          // },
+        },
+      })
 
       return response.status(201).json(group)
     } catch (error: Error | any) {
